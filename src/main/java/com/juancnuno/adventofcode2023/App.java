4e268c093cb6bfdd577dc3cd119ec3be07ba4797
Juan C Nuno <juancnuno@gmail.com>
2023-11-26 10:38:01 -0800

    Commit adventofcode2023

---
+++ src/main/java/com/juancnuno/adventofcode2023/App.java
@@ -0,0 +1,13 @@
+package com.juancnuno.adventofcode2023;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
