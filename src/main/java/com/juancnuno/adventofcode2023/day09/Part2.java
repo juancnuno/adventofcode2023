892100259ad09212f02fac40e9de747c72d7545a
Juan C Nuno <juancnuno@gmail.com>
2024-06-09 14:26:06 -0400

    Solve Day 9 Part 2

--- src/main/java/com/juancnuno/adventofcode2023/day09/Part2.java
+++ src/main/java/com/juancnuno/adventofcode2023/day09/Part2.java
@@ -2,6 +2,8 @@ package com.juancnuno.adventofcode2023.day09;
 
 import java.util.stream.Stream;
 
+import com.juancnuno.adventofcode.AdventOfCode;
+
 public final class Part2 {
 
     private Part2() {
@@ -12,5 +14,9 @@ public final class Part2 {
                 .map(History::new)
                 .mapToInt(History::getPrevious)
                 .sum();
+    }
+
+    public static void main(String[] args) {
+        AdventOfCode.printResultStream(Part2::getPreviousSum);
     }
 }
