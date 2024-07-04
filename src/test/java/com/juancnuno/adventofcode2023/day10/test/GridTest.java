107bbeb7c410e00db17897824558a2ee925ecb0e
Juan C Nuno <juancnuno@gmail.com>
2024-07-04 10:27:28 -0700

    Test getEnclosedTileCount again

--- src/test/java/com/juancnuno/adventofcode2023/day10/test/GridTest.java
+++ src/test/java/com/juancnuno/adventofcode2023/day10/test/GridTest.java
@@ -111,6 +111,29 @@ public final class GridTest {
     }
 
     @Test
+    public void getEnclosedTileCount4() {
+        // Arrange
+        var grid = new Grid("""
+            FF7FSF7F7F7F7F7F---7
+            L|LJ||||||||||||F--J
+            FL-7LJLJ||||||LJL-77
+            F--JF--7||LJLJ7F7FJ-
+            L---JF-JLJ.||-FJLJJ7
+            |F|F-JF---7F7-L7L|7|
+            |FFJF7L7F-JF7|JL---7
+            7-L-JL7||F7|L7F-7F7|
+            L.L7LFJ|||||FJL7||LJ
+            L7JLJL-JLJLJL--JLJ.L
+            """);
+
+        // Act
+        var count = grid.getEnclosedTileCount();
+
+        // Assert
+        assertEquals(10, count);
+    }
+
+    @Test
     public void testToString() {
         // Arrange
         var rows = """
