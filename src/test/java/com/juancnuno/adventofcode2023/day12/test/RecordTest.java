689013456424f90eb47f1371db9e4c731218f26c
Juan C Nuno <juancnuno@gmail.com>
2024-07-05 14:56:41 -0700

    Test getArrangementCount some more

--- src/test/java/com/juancnuno/adventofcode2023/day12/test/RecordTest.java
+++ src/test/java/com/juancnuno/adventofcode2023/day12/test/RecordTest.java
@@ -10,7 +10,7 @@ import com.juancnuno.adventofcode2023.day12.Record;
 public final class RecordTest {
 
     @Test
-    public void getArrangementCount() {
+    public void getArrangementCount1() {
         // Arrange
         var record = new Record("???.### 1,1,3");
 
@@ -19,6 +19,66 @@ public final class RecordTest {
 
         // Assert
         assertEquals(1, count);
+    }
+
+    @Test
+    public void getArrangementCount2() {
+        // Arrange
+        var record = new Record(".??..??...?##. 1,1,3");
+
+        // Act
+        var count = record.getArrangementCount();
+
+        // Assert
+        assertEquals(4, count);
+    }
+
+    @Test
+    public void getArrangementCount3() {
+        // Arrange
+        var record = new Record("?#?#?#?#?#?#?#? 1,3,1,6");
+
+        // Act
+        var count = record.getArrangementCount();
+
+        // Assert
+        assertEquals(1, count);
+    }
+
+    @Test
+    public void getArrangementCount4() {
+        // Arrange
+        var record = new Record("????.#...#... 4,1,1");
+
+        // Act
+        var count = record.getArrangementCount();
+
+        // Assert
+        assertEquals(1, count);
+    }
+
+    @Test
+    public void getArrangementCount5() {
+        // Arrange
+        var record = new Record("????.######..#####. 1,6,5");
+
+        // Act
+        var count = record.getArrangementCount();
+
+        // Assert
+        assertEquals(4, count);
+    }
+
+    @Test
+    public void getArrangementCount6() {
+        // Arrange
+        var record = new Record("?###???????? 3,2,1");
+
+        // Act
+        var count = record.getArrangementCount();
+
+        // Assert
+        assertEquals(10, count);
     }
 
     @Test
